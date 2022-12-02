f9e21b03dfbab39417b654f7e89e851440f2b3d2
Noore Alam Mahin <mahin0w1@gmail.com>
2022-12-02 18:36:33 +0600

    fix toggle

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
 function vdiscripToggle() {
-    var x = document.getElementById("videoDescription");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-function vdiscripToggle() {
     var element = document.getElementById("videoDescription");
     element.classList.toggle("expendDescription");
- }
+}
